bf27257687b5332350a0a8bb76ab022ae11b9ec1
Chris Lyon <chris@flubook.(none)>
2012-05-19 19:41:48 -0400

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectStalker::Application.config.secret_token = '87e283a1ede5a0c368a451752edf345fb25c7613ea0c0814cb6be090051eab7b000010677342e86432df3e341d7853f3e44135f0c2d67b5208e93546ce4fda14'
